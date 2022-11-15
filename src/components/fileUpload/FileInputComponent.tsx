6ec8dda606dfd1e5d976ef911c0630987fa87f19
hugo <hchemillier@juniorisep.com>
2022-11-15 11:56:55 +0100

    fix wording

--- src/components/fileUpload/FileInputComponent.tsx
+++ src/components/fileUpload/FileInputComponent.tsx
@@ -48,8 +48,7 @@ function FileInputComponent({setEmpireInformation}: Props) {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                         d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                 </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or
-                    drag and drop</p>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 font-semibold">Click to upload</p>
                 <p className="text-xs text-gray-500 dark:text-gray-400">JSON</p>
             </div>
             <input id="dropzone-file" type="file" className="hidden" value="" accept="application/JSON" onChange={onFileUpload}/>
