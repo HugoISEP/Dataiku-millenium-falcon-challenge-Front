57cd63c75b93da8095a0f762d918bc3d899e5195
hugo <hchemillier@juniorisep.com>
2022-11-21 21:02:51 +0100

    fix display result issue

--- src/components/displayResult/DisplayResultComponent.tsx
+++ src/components/displayResult/DisplayResultComponent.tsx
@@ -7,7 +7,7 @@ type Props = {
 function DisplayResultComponent({odds}: Props) {
 
     return <div className="h-full flex justify-center items-center">
-        {odds && <h1 className="text-white text-7xl">{`${odds} %`}</h1>}
+        {odds !== undefined && <h1 className="text-white text-7xl">{`${odds} %`}</h1>}
     </div>;
 }
 export default DisplayResultComponent;
