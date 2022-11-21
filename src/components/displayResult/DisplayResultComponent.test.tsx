02c467b40841f0e3e9db567eae8923242a2a6282
hugo <hchemillier@juniorisep.com>
2022-11-21 21:13:18 +0100

    add test for 0 result

--- src/components/displayResult/DisplayResultComponent.test.tsx
+++ src/components/displayResult/DisplayResultComponent.test.tsx
@@ -9,6 +9,11 @@ describe("Test Display Result component", () => {
         const titleElement = screen.getByText(/58 %/i);
         expect(titleElement).toBeInTheDocument();
     });
+    test("Display result 0 in percentage", () => {
+        render(<DisplayResultComponent odds={0} />);
+        const titleElement = screen.getByText(/0 %/i);
+        expect(titleElement).toBeInTheDocument();
+    });
     test("Nothing to display", () => {
         render(<DisplayResultComponent odds={undefined} />);
         const titleElement = screen.queryByText(/(100|[0-9]{1,2})\\s%/i);
